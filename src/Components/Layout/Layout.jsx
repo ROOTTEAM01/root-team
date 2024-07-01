b46c25100981fc2d8c1bca185dec0ed6569e02eb
ROOTTEAM01 <rootteam.development@gmail.com>
2024-07-01 17:04:18 +0400

    header corected

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,16 +1,20 @@
 import {Header} from "./Header/Header";
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import {NewsLetter} from "./NewsLetter/NewsLetter";
 import {Footer} from "./Footer/Footer";
 import '../../CSS/Respons/respons.css'
 
 export let Layout = (props) => {
 
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
+
     let {setRefs, handleNavLinkClick} = props
 
     return (
         <>
-            <Header handleNavLinkClick={handleNavLinkClick} setRefs={setRefs}/>
+            {!isHomePage && <Header handleNavLinkClick={handleNavLinkClick} setRefs={setRefs}/>}
 
             <Outlet/>
 
